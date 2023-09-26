56b4af9871b35342716264b7248ff3c5a3949ea0
iosdevSW <iosdev.sw@gmail.com>
2023-09-27 01:30:50 +0900

    Initial Setting

---
+++ ZeneFit/Common/Extension/UIColor+.swift
@@ -0,0 +1,39 @@
+//
+//  UIColor+.swift
+//  ZeneFit
+//
+//  Created by iOS신상우 on 2023/09/26.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+    static var primaryAlternative: UIColor { return UIColor(named: "Primary_Alternative")! }
+    static var primaryAssistive: UIColor { return UIColor(named: "Primary_Assistive")! }
+    static var primaryNormal: UIColor { return UIColor(named: "Primary_Normal")! }
+    
+    static var secondaryAlternative: UIColor { return UIColor(named: "Secondary_Alternative")! }
+    static var secondaryAssistive: UIColor { return UIColor(named: "Secondary_Assistive")! }
+    static var secondaryNormal: UIColor { return UIColor(named: "Secondary_Normal")! }
+    
+    static var textAlternative: UIColor { return UIColor(named: "Text_Alternative")! }
+    static var textAssistive: UIColor { return UIColor(named: "Text_Assistive")! }
+    static var textDisable: UIColor { return UIColor(named: "Text_Disable")! }
+    static var textNormal: UIColor { return UIColor(named: "Text_Normal")! }
+    static var textStrong: UIColor { return UIColor(named: "Text_Strong")! }
+    
+    static var lineAlternative: UIColor { return UIColor(named: "Line_Alternative")! }
+    static var lineDisable: UIColor { return UIColor(named: "Line_Disable")! }
+    static var lineNormal: UIColor { return UIColor(named: "Line_Normal")! }
+    
+    static var fillAlternative: UIColor { return UIColor(named: "Fill_Alternative")! }
+    static var fillDisable: UIColor { return UIColor(named: "Fill_Disable")! }
+    static var fillNormal: UIColor { return UIColor(named: "Fill_Normal")! }
+    static var fillStrong: UIColor { return UIColor(named: "Fill_Strong")! }
+    
+    static var backgroundPrimary: UIColor { return UIColor(named: "BackgroundPrimary")! }
+    static var backgroundSecondary: UIColor { return UIColor(named: "BackgroundSecondary")! }
+    static var backgroundTertiary: UIColor { return UIColor(named: "BackgroundTertiary")! }
+    
+    static var alert: UIColor { return UIColor(named: "Alert")! }
+}
---
+++ ZeneFit/Common/Extension/UIFont+.swift
@@ -0,0 +1,81 @@
+//
+//  UIFont+.swift
+//  ZeneFit
+//
+//  Created by iOS신상우 on 2023/09/26.
+//
+
+import UIKit.UIFont
+
+extension UIFont {
+    public enum PretendardType: String {
+        case bold = "Bold"
+        case semibold = "SemiBold"
+        case regular = "Regular"
+    }
+
+    static func pretendard(_ type: PretendardType, size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-\(type.rawValue)", size: size)!
+    }
+    
+    static func pretendard(_ type: FontSystemType) -> UIFont {
+        return type.font
+    }
+}
+
+public enum FontSystemType {
+    case headline
+    case title1
+    case title2
+    case label1
+    case label2
+    case label3
+    case label4
+    case label5
+    case body1
+    case body2
+    case chips
+    case caption
+    
+    var lineSpacing: CGFloat {
+        switch self {
+        case .headline: return 48
+        case .title1: return 32
+        case .title2, .label1: return 28
+        case .label2: return 26
+        case .label3, .body1: return 24
+        case .label4, .body2: return 20
+        case .label5, .chips: return 18
+        case .caption: return 14
+        }
+    }
+    
+    var font: UIFont {
+        switch self {
+        case .headline:
+            return .pretendard(.bold, size: 32)
+        case .title1:
+            return .pretendard(.bold, size: 24)
+        case .title2:
+            return .pretendard(.bold, size: 20)
+        case .label1:
+            return .pretendard(.semibold, size: 20)
+        case .label2:
+            return .pretendard(.semibold, size: 18)
+        case .label3:
+            return .pretendard(.semibold, size: 16)
+        case .label4:
+            return .pretendard(.semibold, size: 14)
+        case .label5:
+            return .pretendard(.semibold, size: 12)
+        case .body1:
+            return .pretendard(.regular, size: 16)
+        case .body2:
+            return .pretendard(.regular, size: 14)
+        case .chips:
+            return .pretendard(.regular, size: 12)
+        case .caption:
+            return .pretendard(.regular, size: 10)
+        }
+    }
+}
